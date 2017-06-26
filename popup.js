da68aa3f20531f3b305768209f0823548bf62024
Kelly Lougheed <kellylougheed@gmail.com>
2017-06-25 19:28:46 -0700

    Change buttons to toggle

--- popup.js
+++ popup.js
@@ -1,14 +1,22 @@
 window.onload = function() {
+
   var button = document.getElementById("button");
+  var stopButton = document.getElementById("stop");
+
   button.onclick = function() {
     chrome.extension.sendMessage({
       type: "prettify"
     });
+    button.classList.add("button-on");
+    stopButton.classList.remove("button-on");
   }
-  var stopButton = document.getElementById("stop");
+
   stopButton.onclick = function() {
     chrome.extension.sendMessage({
       type: "stop"
     });
+    stopButton.classList.add("button-on");
+    button.classList.remove("button-on");
   }
+
 }
